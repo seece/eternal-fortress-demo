02966696b2c333825d83f4e7135516441570fb21
Pekka Väänänen <pekka.vaananen@iki.fi>
2020-04-08 23:18:28 +0300

    remove non-debug code

--- shaders/marcher.glsl
+++ shaders/marcher.glsl
@@ -2,7 +2,6 @@
 // the local thread block size; the program will be ran in sets of 16 by 16 threads.
 layout(local_size_x = 16, local_size_y = 16) in;
 
-// simple pseudo-RNG based on the jenkins hash mix function
 uvec4 rndseed;
 void jenkins_mix()
 {
@@ -26,7 +25,6 @@ float rand()
 const int MATERIAL_SKY = -1;
 const int MATERIAL_OTHER = 1;
 
-// uniform variables are global from the glsl perspective; you set them in the CPU side and every thread gets the same value
 uniform int source;
 uniform int frame;
 uniform float secs;
@@ -35,14 +33,6 @@ layout(r32f) uniform image2D zbuffer;
 layout(r8) uniform image2D edgebuffer;
 layout(rgba16f) uniform image2DArray samplebuffer;
 layout(rg8) uniform image2DArray jitterbuffer;
-uniform ivec3 noiseOffset;
-uniform sampler2DArray noiseTextures;
-
-vec3 getNoise(ivec2 coord)
-{
-    return texelFetch(noiseTextures,
-            ivec3((coord.x + noiseOffset.x) % 64, (coord.y + noiseOffset.y) % 64, noiseOffset.z),0).rgb;
-}
 
 struct CameraParams {
     vec3 pos;
@@ -180,62 +170,9 @@ float scene(vec3 p, out int material, float pixelConeSize=1.) {
     return d;
 }
 
-vec3 evalnormal(vec3 p) {
-	vec2 e=vec2(1e-5, 0.f);
-	int m;
-	return normalize(vec3(
-				scene(p + e.xyy,m) - scene(p - e.xyy,m),
-				scene(p + e.yxy,m) - scene(p - e.yxy,m),
-				scene(p + e.yyx,m) - scene(p - e.yyx,m)
-				));
-}
-
 void getCameraProjection(CameraParams cam, vec2 uv, out vec3 outPos, out vec3 outDir) {
 	outPos = cam.pos + cam.dir + (uv.x - 0.5) * cam.right + (uv.y - 0.5) * cam.up;
 	outDir = normalize(outPos - cam.pos);
-}
-
-vec2 shadowMarch(inout vec3 p, vec3 rd, int num_iters, float w, float mint, float maxt) {
-    vec3 ro = p;
-    int i;
-    float omega = 1.3;
-    float t = mint;
-    int mat;
-    float last_d = 0.;
-    float step = 0.;
-    float closest = MAX_DISTANCE;
-
-    for (i = 0; i < num_iters; i++) {
-        float d = scene(ro + t * rd, mat);
-
-        bool sorFail = omega > 1. && (d + last_d) < step;
-        if (sorFail) {
-            step -= omega * step;
-            omega = 1.;
-        } else {
-            step = d * omega;
-        }
-
-        //closest = min(closest, d);
-        closest = min(closest, 0.5+0.5*d/(w*t) );
-
-        last_d = d;
-
-        if (d < 1e-5) {
-            break;
-        }
-
-        if (t >= maxt) {
-            break;
-        }
-
-        t += step;
-    }
-
-    p = ro + t * rd;
-    closest = max(0., closest);
-    //return vec2(t, closest*closest*(3.-2.*closest));
-    return vec2(t, closest);
 }
 
 float depth_march(inout vec3 p, vec3 rd, out int material, out vec2 restart, int num_iters, out int out_iters, float start_t=0., float end_t=20.) {
@@ -293,41 +230,6 @@ float depth_march(inout vec3 p, vec3 rd, out int material, out vec2 restart, int
         debug_child_t = t;
     }
 
-
-    return t;
-}
-
-float simple_march(inout vec3 p, vec3 rd, out int material, out vec2 restart, int num_iters, out int out_iters, float start_t=0., float end_t=20.) {
-    vec3 ro = p;
-    int i;
-    float t = start_t;
-    int mat;
-    material = MATERIAL_OTHER;
-
-    for (i = 0; i < num_iters; i++) {
-        float d = scene(ro + t * rd, mat);
-
-        //float coneWorldRadius = PIXEL_RADIUS * ((t + PROJECTION_PLANE_DIST) / PROJECTION_PLANE_DIST);
-
-        //if (d <= coneWorldRadius) {
-        if (d < 1e-5) {
-            material = mandelbox_material(ro + t * rd);
-            break;
-        }
-
-        t += d;
-
-        if (t >= end_t) {
-            break;
-        }
-    }
-
-    out_iters = i;
-
-    if (t >= end_t) {
-        material = MATERIAL_SKY;
-        return MAX_DISTANCE;
-    }
 
     return t;
 }
@@ -411,23 +313,6 @@ float march(inout vec3 p, vec3 rd, out int material, out vec2 restart, int num_i
     return t;
 }
 
-float sampleAO(vec3 ro, vec3 rd)
-{
-    const int STEPS = 20;
-    const float step = 0.05;
-    float t = step;
-    int mat=0;
-    float obscurance = 0.;
-
-    for (int i=0; i < STEPS; i++) {
-        float d = scene(ro + t * rd, mat);
-        obscurance += max(0., t - d) / t;
-        t += step;
-    }
-
-    return 1. - obscurance / STEPS;
-}
-
 // Maps a ray index "i" into a bin index.
 int tobin(int i)
 {
@@ -471,19 +356,14 @@ vec2 i2ray(int i, out ivec2 squareCoord, out int parentIdx, out int sideLength)
     int size = idim * idim;
     float dim = float(idim);
 
-    int parent_size = size >> 2; // (2 << (b-1));
+    int parent_size = size / 4;
     int parent = int(start - parent_size) + (z/4);
-
-    float margin = 1.0 / (dim);
-    float step = (1.0 - 1. / dim) / dim;
 
     squareCoord = ivec2(coord + vec2(.5));
     parentIdx = parent;
     sideLength = idim;
 
-    //vec2 uv = vec2(margin) + step * vec2(coord);
-    vec2 uv = vec2(0.5/dim) + coord/vec2(dim);
-    //vec2 uv = vec2(coord) / vec2(dim);
+    vec2 uv = vec2(0.5/dim) + coord / vec2(dim);
 
     if (i == 599) {
         debug_i = i;
@@ -495,7 +375,6 @@ vec2 i2ray(int i, out ivec2 squareCoord, out int parentIdx, out int sideLength)
     }
 
      return uv;
-    //return step * vec2(coord);
 }
 
 
@@ -514,14 +393,13 @@ void main() {
         ivec2 squareCoord;
         vec2 squareUV = i2ray(myIdx, squareCoord, parentIdx, sideLength);
 
-        ivec2 pixelCoord = squareCoord; //ivec2(myIdx % res.x, myIdx / res.x);
+        ivec2 pixelCoord = squareCoord;
 
         srand(frame, uint(pixelCoord.x), uint(pixelCoord.y));
         jenkins_mix();
         jenkins_mix();
 
-        vec2 uv = vec2(pixelCoord) / vec2(res);
-        uv = squareUV; // FIXME: requires division by 1023 in splatter to avoid scaling! why
+        vec2 uv = squareUV;
 
         if (any(greaterThan(uv, vec2(1.0)))) {
             continue;
@@ -535,43 +413,14 @@ void main() {
         float parentDepth = jumps[parentIdx];
         parentDepth = 0.; // DEBUG HACK
 
-        // Parent was missed all the rays so bail out.
-        /* if (parentDepth >= MAX_DISTANCE) {
-            jumps[myIdx] = parentDepth;
-            continue;
-        } */
-
         CameraParams cam = cameras[1];
         vec3 p, dir;
         getCameraProjection(cam, uv, p, dir);
 
-        //PIXEL_RADIUS = .5 * length(cameras[1].right) / res.x;
         vec3 rp = p - cam.pos;
         PROJECTION_PLANE_DIST = length(rp);
         NEAR_PLANE = length(cam.dir);
-        //PIXEL_RADIUS = sqrt(2.) * .5 * length(cam.right) / float(sideLength);
         PIXEL_RADIUS = sqrt(2.) * length(cam.right) / float(sideLength);
-        //PIXEL_RADIUS += 0.2 * (1./sideLength);
-        /*
-        float one_pixel_radius = sqrt(2.) * length(cam.right) / float(sideLength);
-        vec3 corner = p - (cam.right + cam.up) / float(sideLength); // half a pixel bias
-        float angle = acos(dot(normalize(corner), rp/PROJECTION_PLANE_DIST));
-        PIXEL_RADIUS = sin(angle) * PROJECTION_PLANE_DIST;
-        */
-
-        /*
-        {
-            vec3 p2, dir2;
-            getCameraProjection(cam, uv + vec2(1) / sideLength, p2, dir2);
-            vec3 a = p - cam.pos;
-            vec3 b = p2 - cam.pos;
-            float cosine = dot(normalize(a), normalize(b));
-            float angle = acos(cosine);
-            float pixelSideLengthWorld = length(cam.right) / float(sideLength);
-            float tp = length(a);
-            PIXEL_RADIUS = pixelSideLengthWorld * sin(angle) * tp;
-        }
-        */
 
         int hitmat = MATERIAL_SKY;
         vec2 restart;
@@ -618,50 +467,7 @@ void main() {
         if (hitmat == MATERIAL_SKY) {
             color = skyColor;
         } else {
-            vec3 normal = evalnormal(p);
-            vec3 to_camera = normalize(cam.pos - p);
-            vec3 to_light = normalize(vec3(-0.5, -1.0, 0.7));
-
-            // HACK: Disable shading
-            if (false) {
-                vec3 shadowRayPos = p + to_camera * 1e-4;
-                const float maxShadowDist = 10.;
-                vec2 shadowResult = shadowMarch(shadowRayPos, to_light, 400, 9e-2, 0.01, maxShadowDist);
-                float sun = min(shadowResult.x, maxShadowDist) / maxShadowDist;
-                //sun = max(0., shadowResult.y - 0.1);
-                //sun = (sun+0.1) * shadowResult.y;
-
-                //float sun = min(1.0, shadowResult.y*1e3);
-                sun = pow(sun, 2.);
-
-                float ambient = sampleAO(p, normal);
-                ambient = pow(ambient, 2.0);
-
-                float facing = max(0., dot(normal, to_light));
-
-                vec3 base = vec3(.5) + .5*vec3(sin(hitmat + vec3(0., .5, 1.)));
-
-
-                //color = base * sun * vec3(facing);
-                color = vec3(sun);
-                vec3 skycolor = vec3(0.5, 0.7, 1.0);
-                vec3 suncolor = vec3(1., 0.8, 0.5);
-                color = base * (ambient * skycolor + facing * sun * suncolor);
-                color = clamp(color, vec3(0.), vec3(10.));
-                //color = vec3(0.5)+.5*cos( 10*vec3(iters)/600.  + vec3(0., 0.5, 1.));
-            } else {
-                color = vec3(pow(zdepth/10., 5.), 0., 0.); // * vec3(0., 1., 0.);
-            }
-        }
-
-        //color = vec3(iters/800.);
-        //color = vec3(pow(zdepth/10., 4.));
-        //color = vec3(.0)+.5*sin(vec3(zdepth*40.) + vec3(0., 0.5, 1.));
-        if (zdepth == 0.){
-            color = vec3(1., 0., 0.);
-        }
-        if (parentDepth > 0 && zdepth < parentDepth) {
-            color = vec3(0., 1., 1.);
+            color = vec3(pow(zdepth/10., 5.), 0., 0.); // * vec3(0., 1., 0.);
         }
 
         if (true /* DEBUG HACK */ || hitmat != MATERIAL_SKY) {
